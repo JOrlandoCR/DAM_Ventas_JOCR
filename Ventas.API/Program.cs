f101b8a8ec30f13d40854fe3b1bbc8069a31f8ef
JOrlando CR <21030582@itesa.edu.mx>
2025-03-07 15:11:20 -0600

    Version_02: Se creo la bd con EFC

--- Ventas.API/Program.cs
+++ Ventas.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Ventas.API.Data;
+
 namespace Ventas.API
 {
     public class Program
@@ -13,6 +16,8 @@ namespace Ventas.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name = LocalHostConnection"));
 
             var app = builder.Build();
 
